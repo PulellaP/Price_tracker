f9dcc2533311f1c0fc99921787b6163ddeb3e5b5
PulellaP <ppulella@gmail.com>
2023-12-07 06:14:30 -0500

    starting prject

---
+++ scrape.py
@@ -0,0 +1,20 @@
+# Web Scraping
+# - Use a Python library like BeautifulSoup or Scrapy to scrape product prices from eBay, Amazon, Alibaba, etc.
+# - Be mindful of the terms of service for each platform to ensure compliance.
+
+# Data Entry
+# - Organize the scraped data into a structured format, such as a pandas DataFrame.
+# - Include relevant information like product name, price, timestamp, and platform.
+
+# Data Storage
+# - Choose a storage solution for your data, such as SQLite, MySQL, or MongoDB.
+# - Write Python code to store the structured data in the chosen database.
+
+# Data Analysis
+# - Utilize pandas and numpy for basic data analysis.
+# - Calculate statistical measures like mean, median, and standard deviation of prices.
+
+# Excel Integration
+# - Use the openpyxl or xlrd libraries to read and write Excel files in Python.
+# - Create Excel sheets to display the scraped data and the results of data analysis.
+# - Include graphs and charts using Microsoft Excel formulas and features.
