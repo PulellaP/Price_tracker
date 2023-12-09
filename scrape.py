a9dd0d02b5d5261bbb36f8800fd510f672ab0de3
PulellaP <ppulella@gmail.com>
2023-12-08 20:28:48 -0500

    open browser to search on amazon

--- scrape.py
+++ scrape.py
@@ -1,6 +1,21 @@
+# Import Statements
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 # Web Scraping
 # - Use a Python library like BeautifulSoup or Scrapy to scrape product prices from eBay, Amazon, Alibaba, etc.
 # - Be mindful of the terms of service for each platform to ensure compliance.
+
+# create webdriver object
+driver = webdriver.Firefox()
+# get google.co.in
+driver.get("https://www.amazon.com")
+# get element of search box
+searchbox_element = driver.find_element(By.ID, "twotabsearchtextbox")
+# send keys
+searchbox_element.send_keys("Phone")
+searchbox_element.send_keys(Keys.ENTER)
 
 # Data Entry
 # - Organize the scraped data into a structured format, such as a pandas DataFrame.
